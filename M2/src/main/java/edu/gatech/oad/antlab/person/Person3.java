ab0e2de210be0dfeeeaf41b655e08fdc6cd6d93d
tahmad08 <42183137+tahmad08@users.noreply.github.com>
2018-09-03 21:19:47 -0400

    added the required method

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+        String backwards = "";
+        for( int x = input.length() - 1 ; x >= 0 ; x-- ) {
+            backwards = backwards + input.charAt(x);
+        }
+        return backwards;
 	}
 }
