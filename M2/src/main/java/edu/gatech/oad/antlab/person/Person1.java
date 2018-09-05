f8285bacd5bffd47d6b86ad0ba9ecef410f7c5d3
Shahnaaz Khader <shahnaazkhader@lawn-128-61-37-77.lawn.gatech.edu>
2018-09-04 22:59:06 -0400

    modified person1 file

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,11 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		//Person 1 put your implementation here
+		String chars = input.substring(0, 2);
+		String toReturn = input.substring(2);
+		toReturn += chars;
+	  return toReturn;
 	}
 	
 	/**
