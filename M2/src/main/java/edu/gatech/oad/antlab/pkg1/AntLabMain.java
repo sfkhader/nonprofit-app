555cae0d02a1067168ebba7e436911aba839bd28
tahmad08 <42183137+tahmad08@users.noreply.github.com>
2018-09-07 21:40:43 -0400

    Edited P3 part of AntlabMain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,8 +67,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("skhan340");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Tahirah Ahmad");
+		  toPrint += p3.toString("tahmad8");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Rwan Hayek");
