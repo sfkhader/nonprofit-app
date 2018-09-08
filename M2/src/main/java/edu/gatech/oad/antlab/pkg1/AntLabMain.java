f616eec4c1a43728f48c255001bf2b81fc248715
StephenKamali <stephen.kamali@gmail.com>
2018-09-07 21:37:20 -0400

    Edited Person5 portion of AntLabMain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -75,8 +75,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("rhayek3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Stephen Kamali");
+                  toPrint += p5.toString("skamali6");
 		  
         System.out.println(toPrint);
         
