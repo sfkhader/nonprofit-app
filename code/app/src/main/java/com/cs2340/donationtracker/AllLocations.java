7e0c18ef6bf27554c5bb0487cd3e174ad5a17ae3
Shahnaaz Khader <shahnaazkhader@lawn-128-61-39-118.lawn.gatech.edu>
2018-10-17 23:46:48 -0400

    added location pages

--- code/app/src/main/java/com/cs2340/donationtracker/AllLocations.java
+++ code/app/src/main/java/com/cs2340/donationtracker/AllLocations.java
@@ -4,21 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.EditText;
 
 
-public class AppActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bLogout;
+public class AllLocations extends AppCompatActivity implements View.OnClickListener{
+    Button information;
+    Spinner locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.activity_all_locations);
 
-        bLogout = (Button) findViewById(R.id.bLogout);
+        information = (Button) findViewById(R.id.information);
+        locations = (Spinner) findViewById(R.id.spinner);
 
-        bLogout.setOnClickListener(this);
+        information.setOnClickListener(this);
     }
 
     @Override
@@ -31,10 +34,6 @@ public class AppActivity extends AppCompatActivity implements View.OnClickListen
         if (v.getId() == R.id.bLogout) {
 
             Intent goback = new Intent(this, LoginPage.class);
-            startActivity(goback);
-            return;
-        } else if (v.getId() == R.id.locations) {
-            Intent goback = new Intent(this, AllLocations.class);
             startActivity(goback);
             return;
         }
---
+++ code/app/src/main/java/com/cs2340/donationtracker/Location.java
@@ -0,0 +1,4 @@
+package com.cs2340.donationtracker;
+
+public class Location {
+}
