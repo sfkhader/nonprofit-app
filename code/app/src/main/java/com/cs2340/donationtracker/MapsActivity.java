341f142ce687f337a8542dfb28060be3167fb096
StephenKamali <stephen.kamali@gmail.com>
2018-11-08 20:45:51 -0500

    Fixed bug with MapsActivity

--- code/app/src/main/java/com/cs2340/donationtracker/MapsActivity.java
+++ code/app/src/main/java/com/cs2340/donationtracker/MapsActivity.java
@@ -10,18 +10,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
